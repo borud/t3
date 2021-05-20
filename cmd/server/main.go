b269571182538e88869a0cf88e60cdf7f2f8ec93
Bjorn Borud <borud@lab5e.org>
2021-05-20 16:52:20 +0200

    adding interceptor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/borud/t3/pkg/apipb"
@@ -16,6 +17,7 @@ import (
 )
 
 var svc *service.Service
+var waitForGRPCServerToStart sync.WaitGroup
 
 const (
 	// The address to which our gRPC server listens
@@ -28,6 +30,8 @@ const (
 func main() {
 	// Create the service
 	svc = service.New()
+
+	waitForGRPCServerToStart.Add(1)
 
 	go grpcService()
 	go restService()
@@ -47,18 +51,33 @@ func grpcService() {
 	}
 
 	// Create the gRPC server
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(service.LoggingServerInterceptor))
 
 	// Register the service with the server
 	apipb.RegisterMapsServer(grpcServer, svc)
 
 	// Start the server.  This call blocks until the server terminates.
 	log.Printf("Starting gRPC server on %s", grpcListenAddress)
+
+	// Signal that it is safe to connect to the gRPC server now
+	waitForGRPCServerToStart.Done()
+
+	// Start the gRPC server.  This will block.
 	grpcServer.Serve(listenSocket)
 }
 
 // Start the REST service
 func restService() {
+	// Before trying to connect to the server we need to wait until it has started.
+	waitForGRPCServerToStart.Wait()
+
+	// create a client that talks to the local gRPC server
+	conn, err := grpc.Dial(grpcListenAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("error dialing gRPC server: %v", err)
+	}
+	client := apipb.NewMapsClient(conn)
+
 	// Create new grpc-gateway mux
 	mux := runtime.NewServeMux()
 
@@ -66,7 +85,7 @@ func restService() {
 	ctx := context.Background()
 
 	// Register the handler for Maps
-	err := apipb.RegisterMapsHandlerServer(ctx, mux, svc)
+	err = apipb.RegisterMapsHandlerClient(ctx, mux, client)
 	if err != nil {
 		log.Fatalf("unable to register MapsHandlerServer: %v", err)
 	}
---
+++ pkg/service/interceptor.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+// LoggingServerInterceptor logs the incoming request, calls the handler it was intended for
+// logs the response it got and then returns the response and any error.
+func LoggingServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	// This line is logged before the request
+	log.Println()
+	log.Printf("[BEFORE] server=%v method=%s req=%+v", info.Server, info.FullMethod, req)
+
+	// Remember to call the original request
+	response, err := handler(ctx, req)
+
+	// This line is logged after we handed off the request to where it was supposed to go
+	log.Printf("[ AFTER] response=%+v", response)
+
+	// Now return the response and error
+	return response, err
+}
